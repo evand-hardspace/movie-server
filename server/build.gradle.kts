f499e74d387604dbe3e211a9ff62f8b93e109498
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2026-05-04 19:07:56 +0200

    Fixed db issues

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -8,7 +8,7 @@ group = "com.evandhardspace.movie-server"
 version = "1.0.0-SNAPSHOT"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass = "com.evandhardspace.movie.server.MainKt"
 }
 
 kotlin {
